2b4fb13ac3df6372fe4188c28c6fd0f0529e0c05
Andy Gregorowicz <andy@mitre.org>
2012-03-29 16:14:49 -0400

    Generated basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleRhex::Application.config.secret_token = '7b85d1e61e1d496835f3e7a813f7eff04ee54f72deb24fa240d016adae2bad834ea0e320adfb1dfdf838954ffb67a886db2be8a769fb675085e3b7eff6efbd37'
